81f75d0f2c233b8652934775996871f63fd606bc
Erick Bernardo <erickvictor@gmail.com>
2020-06-01 16:18:10 -0400

    Insert connect mongodb

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose')
 const Joi = require('@hapi/joi')
 const genres = require('./routes/genres')
 const express = require('express')
 const app = express()
+
+const url = 'mongodb://mosh:db987654@ds357708.mlab.com:57708/playground'
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+mongoose.connect(url, options)
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err))
 
 app.use(express.json())
 app.use('/api/genres', genres)
