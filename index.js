e35987dd12fa04546168675527d0ea5c7d0e357b
Erick Bernardo <erickvictor@gmail.com>
2020-06-03 18:55:30 -0400

    Add app use movie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const genres = require('./routes/genres')
 const customers = require('./routes/customers')
+const movies = require('./routes/movies')
 const express = require('express')
 const app = express()
 
@@ -17,6 +18,7 @@ mongoose.connect(url, options)
 app.use(express.json())
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
+app.use('/api/movies', movies)
 
 
 const port = process.env.PORT || 3000
