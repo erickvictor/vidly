96a08f309ade3d36f9a468620294cb597033a68f
Erick Bernardo <erickvictor@gmail.com>
2020-06-04 16:34:49 -0400

    Role Based Authorization

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
 const {Genre, validate} = require('../models/genre')
 const mongoose = require('mongoose')
 const express = require('express')
@@ -44,7 +45,7 @@ router.put('/:id', async (req, res) => {
   
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin],  async (req, res) => {
   if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     const genre = await Genre.findByIdAndRemove(req.params.id)
     res.send(genre)
