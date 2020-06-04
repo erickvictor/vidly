ad597cf2f334de45300c8d56b0c7f03875c1a17c
Erick Bernardo <erickvictor@gmail.com>
2020-06-03 22:20:33 -0400

    Insert route post Users and Bcrypt

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
+const users = require('./routes/users')
 const express = require('express')
 const app = express()
+
+mongoose.set('useCreateIndex', true)
 
 const url = 'mongodb://mosh:db987654@ds141208.mlab.com:41208/playground'
 const options = {
@@ -23,6 +26,7 @@ app.use('/api/genres', genres)
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
+app.use('/api/users', users)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
