a6cdca0bf9ace5c38d1e2f21ffaf7a3512c7eabc
Erick Bernardo <erickvictor@gmail.com>
2020-06-11 19:40:30 -0400

    Unit Test and Integration Test

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require('../middleware/validateObjectId')
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
 const {Genre, validate} = require('../models/genre')
@@ -6,7 +7,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  throw new Error('Could not get the genres.')
+  // throw new Error('Could not get the genres.')
   const genres = await Genre.find().sort('name')
   res.send(genres)
 })
@@ -21,50 +22,33 @@ router.post('/', auth , async (req,res) => {
   res.send(genre)
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
   const { error } = validate(req.body)
   if(error) return res.status(400).send(error.details[0].message)
 
-  if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
-      new: true
-  })
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  });
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
   res.send(genre)
-  } else {
-    return res.status(404).send('The genre with the given ID was not found.')
-  }
-
-  // const genre = await Genre.findOneAndUpdate(req.params.id, { name: req.body.name }, {new: true}, (err, doc) => {
-  //   if (err) {
-  //       console.log("Something wrong when updating data!");
-  //   }
-  //   console.log(doc);
-  // })
-
-  // if (!genre) return res.status(404).send('The genre with the given ID was not found.')
-
-  
 })
 
-router.delete('/:id', [auth, admin],  async (req, res) => {
-  if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    const genre = await Genre.findByIdAndRemove(req.params.id)
-    res.send(genre)
-  } else {
-    return res.status(404).send('The genre with the given ID was not found.')
-  }
-  // if (!genre) return res.status(404).send('The genre with the given ID was not found.')
+router.delete('/:id', [auth, admin, validateObjectId],  async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id)
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.')
+
+  res.send(genre)
 })
 
-router.get('/:id', async (req, res) => {
-  if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    const genre = await Genre.findById(req.params.id)
-    res.send(genre)
-  } else {
-    return res.status(404).send('The genre with the given ID was not found.')
-  }
-  // if (!genre) return res.status(404).send('The genre with the given ID was not found.')
-  
+router.get('/:id', validateObjectId, async (req, res) => {
+  const genre = await Genre.findById(req.params.id)
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.')
+
+  res.send(genre)
 })
 
 module.exports = router
--- startup/db.js
+++ startup/db.js
@@ -1,15 +1,16 @@
 const winston = require('winston')
 const mongoose = require('mongoose')
+const config = require('config')
 
 module.exports = function() {
-  const url = 'mongodb://mosh:db987654@ds141208.mlab.com:41208/playground'
+  const url = config.get('db')
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
   
 mongoose.connect(url, options)
-  .then(() => winston.info('Connected to MongoDB...'))
+  .then(() => winston.info(`Connected to ${url}...`))
 
 mongoose.set('useCreateIndex', true)
 }
